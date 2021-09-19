9417eb255035409e0974d077d590b959f9fba349
rabinkhanal <rabinkhanal2055@gmail.com>
2021-09-19 23:20:26 +0545

    Commit

---
+++ app/src/main/java/com/rabin/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.rabin.shopping.Prevalent;
+
+import com.rabin.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
